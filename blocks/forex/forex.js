2e78ad3fde4750064a06184cde99b94eb90ab3b1
vvedagiri1 <161562723+vvedagiri1@users.noreply.github.com>
2026-02-10 15:46:05 +0530

    Update forex.js

--- blocks/forex/forex.js
+++ blocks/forex/forex.js
@@ -7,14 +7,14 @@ export default async function decorate(block) {
   const location = 'ny';
   const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`;
   const options = {};
-  //const response = await fetch(url,options);
-  //const data = await response.json();
+  const response = await fetch(url,options);
+  const data = await response.json();
 
   
   //const url = `https://20092-securbankdemo-stage.adobeio-static.net/api/v1/web/dx-excshell-1/forex?baseCurrency=${firsttag}`;
   //const options = {};
-  const forexReq = await fetch(url, options);
-  const index = await forexReq.json();
+  //const forexReq = await fetch(url, options);
+  //const index = await forexReq.json();
   // 3. Create HTML structure
   const weatherHTML = `
     <div class="weather-card">
